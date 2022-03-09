e8a9ec64cbaa2e5dfaa904f25add4439de695ee3
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-09 12:24:11 -0500

    [Fix] Change returns in playRound function polish code prior testing

--- game.js
+++ game.js
@@ -9,10 +9,9 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection)
 {
   let objects = ["Paper","Scissors","Rock"];
-  let playerInput = prompt("Please enter your choice: Paper, Scissors, Rock")
   let playerChoice;
 
-  switch (playerInput.toLowercase()) {
+  switch (playerSelection.toLowercase()) {
     case "paper":
     playerChoice = 1;
     break;
@@ -22,17 +21,21 @@ function playRound(playerSelection, computerSelection)
     case "rock":
     playerChoice = 3;
     default:
-    alert("Wrong choice / Typo");
+    alert("Invalid choice");
   }
   if(playerChoice===computerSelection){
-    console.log("Draw!");
+    return "Draw!";
   }
 
   if((computerSelection===1 && playerChoice===3)||(computerSelection===2 && playerChoice===1))
   {
-    return console.log("You win!" + objects[playerSelection-1] + "beats " + objects[computerSelection-1]);
+    return "You win!" + objects[playerSelection-1] + "beats " + objects[computerSelection-1];
   }else {
-    return console.log("You lose!" + objects[computerSelection-1] + "beats " + objects[playerSelection-1]);
+    return "You lose!" + objects[computerSelection-1] + "beats " + objects[playerSelection-1];
   }
-
 }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
