b9c4a60ba754355e30544ec010dd5aa95fe2ff3c
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-09 11:57:03 -0500

    [Feature] Merge previous playerPlay and playRound functions

--- game.js
+++ game.js
@@ -6,64 +6,33 @@ function computerPlay(){
   return Math.floor(Math.random()*3);
 }
 
-function playerPlay(){
+function playRound(playerSelection, computerSelection)
+{
+  let objects = ["Paper","Scissors","Rock"];
   let playerInput = prompt("Please enter your choice: Paper, Scissors, Rock")
   let playerChoice;
 
-  switch (playerInput) {
-    case "Paper":
-        playerChoice = 1;
-      break;
-    case "Scissors":
-      playerChoice = 2;
-      break;
-    case "Rock":
-      playerChoice = 3;
+  switch (playerInput.toLowercase()) {
+    case "paper":
+    playerChoice = 1;
+    break;
+    case "scissors":
+    playerChoice = 2;
+    break;
+    case "rock":
+    playerChoice = 3;
     default:
-      alert("Wrong choice / Typo");
+    alert("Wrong choice / Typo");
   }
-  return playerChoice;
-}
-
-function playRound(playerSelection, computerSelection)
-{
-  let playerWins = false;
-
-  if(playerSelection===computerSelection){
+  if(playerChoice===computerSelection){
     console.log("Draw!");
   }
 
-  if((computerSelection===1 && playerSelection===3)||(computerSelection===2 && playerSelection===1))
+  if((computerSelection===1 && playerChoice===3)||(computerSelection===2 && playerChoice===1))
   {
-    playerWins = false;
+    return console.log("You win!" + objects[playerSelection-1] + "beats " + objects[computerSelection-1]);
   }else {
-    playerWins = True;
-  }
-  return playerWins;
-}
-
-function game(){
-
-  let objects = ["Paper","Scissors","Rock"];
-
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 4; i++) {
-
-    if (playRound(playerPlay(),computerPlay())) {
-      return console.log("You win!" + objects[playerSelection-1] + "beats " + objects[computerSelection-1]);
-
-    }else {
-      return console.log("You lose!" + objects[computerSelection-1] + "beats " + objects[playerSelection-1]);
-    }
-  }
-
-  if(playerScore>computerScore)
-  {
-    console.log("Winner!");
-  }else{
-    console.log("Loser :(");
+    return console.log("You lose!" + objects[computerSelection-1] + "beats " + objects[playerSelection-1]);
   }
 
 }
