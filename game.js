67a6fb54e973991ee4cb1fe74dc998a54c68b4f9
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-09 10:21:24 -0500

    [Feature] Add player's move function

--- game.js
+++ game.js
@@ -1,12 +1,31 @@
-function computerPlay(){
+  function computerPlay(){
 
   //Redefine function that matches numbers like | 1 = Paper | 2 = Scissors | 3 = Rock |
   return Math.floor(Math.random()*3);
 }
 
+function playerPlay(){
+  let playerInput = prompt("Please enter your choice: Paper, Scissors, Rock")
+  let playerChoice;
+
+  switch (playerInput) {
+    case "Paper":
+        playerChoice = 0;
+      break;
+    case "Scissors":
+      playerChoice = 1;
+      break;
+    case "Rock":
+      playerChoice = 2;
+    default:
+      alert("Wrong choice / Typo");
+  }
+
+  return playerChoice;
+}
+
 function playRound(playerSelection, computerSelection)
 {
-
   let objects = ["Paper","Scissors","Rock"];
   bool playerWins = false;
 
@@ -17,7 +36,7 @@ function playRound(playerSelection, computerSelection)
   if((computerSelection===1 && playerSelection===3)||(computerSelection===2 && playerSelection===1))
   {
     playerWins = false;
-  }else if ((computerSelection===3 && playerSelection===1)||(computerSelection===1 && playerSelection===2)) {
+  }else {
     playerWins = True;
   }
 
@@ -26,5 +45,8 @@ function playRound(playerSelection, computerSelection)
   }else {
     return console.log("You lose!" + objects[computerSelection-1] + "beats " + objects[playerSelection-1]);
   }
+}
+
+function game(){
 
 }
